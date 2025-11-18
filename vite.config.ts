b04978483a132233fdc35498b43a7a1c07eab5c3
andrew <1144098817@qq.com>
2025-11-18 18:33:49 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import updateManifestPlugin from "./plugins/update-manifest";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./", // 将根路径换成相对路径
   plugins: [react(), updateManifestPlugin()],
 });
